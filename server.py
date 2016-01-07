755429c72cf7acbd33b2ac88c3549e9981520479
Kamila Součková <kamila@ksp.sk>
2016-01-07 21:13:52 +0100

    add random "redis works" test

--- server.py
+++ server.py
@@ -1,14 +1,21 @@
 import cherrypy
+import redis
+
 import os
 
 class App(object):
+    def __init__(self, db_conn):
+        self.db_conn=db_conn
+
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        return 'foo: '+str(db.smembers('foo'))
+
+db = redis.from_url(os.environ.get('REDISCLOUD_URL'))
 
 cherrypy.config.update({
     'server.socket_host': '0.0.0.0',
     'server.socket_port': int(os.environ.get('PORT', '5000')),
     'tools.proxy.on': True,
 })
-cherrypy.quickstart(App())
+cherrypy.quickstart(App(db_conn=db))
