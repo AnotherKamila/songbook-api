d0a311a647cd6ac863f98ce5e37faeb25638e9ca
Kamila Souckova <kamila@ksp.sk>
2017-04-19 23:58:14 +0200

    add recaptcha verification (TODO does it work? :D)

--- songbook/auth.py
+++ songbook/auth.py
@@ -1,5 +1,29 @@
+import os
+from datetime import datetime, timezone
+
+import iso8601
+import requests
+
 from .ref import refjoin
 from .utils import to_utf8
+
+RECAPTCHA_DIFF = 60  # seconds
+
+def recaptcha(token):
+    r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                  data={
+                      'secret': os.environ['RECAPTCHA_SECRET'],
+                      'response': token,
+                  })
+    print(r.json())
+    if r.json()['success']:
+        ts = iso8601.parse_date(r.json()['challenge_ts'])
+        return abs((datetime.now(timezone.utc) - ts).total_seconds()) < RECAPTCHA_DIFF
+
+def with_recaptcha(fn):
+    def decorated(d, db):
+        if 'recaptcha' in d and recaptcha(d['recaptcha']):
+            return fn(d, db)
 
 def passcode(d, db):
     # TODO hash passcodes
@@ -12,9 +36,11 @@ def google(d, db):
     # TODO verify tokens
     raise NotImplemented
 
+
 types = {
     'passcode': passcode,
     'google': google,
+    'script': ...
 }
 def user(d, db):
     """Authenticates a user, or returns None if auth failed.
